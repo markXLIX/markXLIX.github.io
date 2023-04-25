2e9a2ba875511d43c4bbf1682ecfb8847d2fb4b3
Mark Shellenberger <mshellenberger@flexion.us>
2023-04-25 10:12:39 -0400

    Formatting and CSS

--- MGT/DiffMGT.py
+++ MGT/DiffMGT.py
@@ -10,9 +10,9 @@ output_dir = 'MGT/Outputs/'
 
 # open .tsv file(s)
 #nih_initial = open('NIH_testing.tsv', encoding='utf-8').read().split('\n')
-input1_initial = open('C:\GitHub\markXLIX.github.io\MGT\CDC_testing.tsv',
+input1_initial = open('\GitHub\markXLIX.github.io\MGT\CDC_testing.tsv',
                       encoding='utf-8').read().split('\n')
-input2_initial = open('C:\GitHub\markXLIX.github.io\MGT\A_SSA English-Spanish.tsv',
+input2_initial = open('\GitHub\markXLIX.github.io\MGT\A_SSA English-Spanish.tsv',
                       encoding='utf-8').read().split('\n')
 # END ADJUST for source documents
 
@@ -26,33 +26,34 @@ input2_initial = open('C:\GitHub\markXLIX.github.io\MGT\A_SSA English-Spanish.ts
 # END COMMENTS
 
 
-def single_sheet_html(english_term_exists, no_match_list_input1_item, no_match_list_input2_item, terms_match,
-                      english_count, input1_count, input2_count, match_count):
+#            <style>.row_highlight {background-color: gray;}</style>
+
+def single_sheet_html(headers, no_match_list_input1_item, no_match_list_input2_item, terms_match, input1_count, input2_count, match_count):
     html_start = f"""<html lang='en'>
             <meta charset='UTF-8'>
             <title>Page Title</title>
             <meta name='viewport' content='width=device-width,initial-scale=1'>
+            <head><link rel="stylesheet"  type="text/css" href="..\DiffMGT.css"></head>
             <body>
                 <h1>TEST</h1>
                 """
     html_counter = single_sheet_html_count(
-        english_count, input1_count, input2_count, match_count)
+        input1_count, input2_count, match_count)
     html_body = single_sheet_html_body(
-        english_term_exists, no_match_list_input1_item, no_match_list_input2_item, terms_match)
+        headers, no_match_list_input1_item, no_match_list_input2_item, terms_match)
     html_end = f"""
             </body>
         </html>"""
-    return html_start + html_end
+    return html_start + html_counter + html_body + html_end
 
 
-def single_sheet_html_count(english_count, input1_count, input2_count, match_count):
+def single_sheet_html_count(input1_count, input2_count, match_count):
     html = f"""<div>
         <p>
                 <ul>
-                    <li>{input1_count}: Count of No Match - First Input</li>
-                    <li>{input2_count}: Count of No Match - Second Input</li>
-                    <li>{english_count}: Count of English Match - No Spanish Match</li>
-                    <li>{match_count}: Count of English and Spanish Terms Match</li>
+                    <li><a href='#input1_no_match'>{input1_source} - No Match to {input2_source}</a> - {input1_count} hits</li>
+                    <li><a href='#input2_no_match'>{input2_source} - No Match to {input1_source}</a> - {input2_count} hits</li>
+                    <li><a href='#match'>Inputs Match</a> - {match_count} hits</li>
                 </ul>
             </p>
     </div>"""
@@ -61,17 +62,22 @@ def single_sheet_html_count(english_count, input1_count, input2_count, match_cou
 #DEBUG - This is not working
 
 
-def single_sheet_html_body(english_term_exists, no_match_list_input1_item, no_match_list_input2_item, terms_match):
-    table = "<table><tr><th>Input1-English</th><th>Input1-Spanish</th><th>Input2-English</th><th>Input-Spanish</th></tr>"
-    for row in english_term_exists:
-        term_row = f"""<tr><td>{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td><td>{row[3]}</td></tr>"""
-        table = table + term_row
-    table = table + "</table>"
-    return table
-
-
-def count_output_rows(output):
-    return len(output)
+def single_sheet_html_body(headers, no_match_list_input1_item, no_match_list_input2_item, terms_match):
+    table_1 = f"""<div id='input1_no_match'><table><tr><th>{headers[0]}</th><th>{headers[1]}</th><th>{headers[2]}</th><th>{headers[3]}</th></tr>"""
+    counter = 0
+    highlight = ''
+    for row in no_match_list_input1_item:
+        # only highlight the odd rows
+        if (counter % 2 == 0):
+            highlight = "class='row_highlight'"
+        term_row = f"""<tr {highlight}><td>{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td><td>{row[3]}</td></tr>"""
+        table_1 = table_1 + term_row
+        counter += 1
+        highlight = ''
+    table_1 += "</table></div>"
+    # reset counter for next table
+    counter = 0
+    return table_1
 
 
 input2_list = []
@@ -86,7 +92,6 @@ for i in input2_initial:
 terms_match = []
 no_match_list_input1_item = []
 no_match_list_input2_item = []
-english_term_exists = []
 last_english_term_input1_item = ''
 last_english_term_input2_item = ''
 
@@ -118,13 +123,6 @@ for input1_item in input1_list:
                 # break out of the loop through list2
                 match_found = True
                 break
-            # else:
-                # a = [input1_item[0], input1_item[1],
-                #     input2_item[0], input2_item[1]]
-                # english_term_exists.append(a)
-                #match_found = True
-                # break
-
     # if no match was found, add the row to the no_match_list variable
     if not match_found:
         a = [input1_item[0], input1_item[1], None, None]
@@ -151,23 +149,19 @@ for input2_item in input2_list:
         no_match_list_input2_item.append(a)
 
 # count of each output variable
-count_english_term_exists = len(english_term_exists)
 count_no_match_list_input1 = len(no_match_list_input1_item)
 count_no_match_list_input2 = len(no_match_list_input2_item)
 count_terms_match = len(terms_match)
 
 # print the results to screen
-print("\n", "English Terms Exists In Both, Spanish Does Not Match:")
-for row in english_term_exists:
-    print(row)
-print("Match count:", count_english_term_exists)
-
-print("\n", input1_source, "English Term does not exist in", input2_source, ": ")
+print("\n", input1_source,
+      "English/Spanish pair does not exist in", input2_source, ": ")
 for row in no_match_list_input1_item:
     print(row)
 print("No match count:", count_no_match_list_input1)
 
-print("\nNIH English Term does not exist in CDC:")
+print("\n", input2_source,
+      "English/Spanish pair does not exist in", input1_source, ": ")
 for row in no_match_list_input2_item:
     print(row)
 print("No match count:", count_no_match_list_input2)
@@ -181,8 +175,6 @@ print("Match count:", count_terms_match)
 headers = ['CDC English', 'CDC Spanish', 'NIH English', 'NIH Spanish']
 
 # names of files to write
-English_Not_Spanish = output_dir + input1_source + '_' + \
-    input2_source + '__' + 'EnglishMatchesNotSpanish.tsv'
 CDC_English_Not_NIH_Spanish = output_dir + input1_source + '_' + input2_source + \
     '__' + input2_source + 'EnglishNot' + input1_source + 'Spanish.tsv'
 CNIH_English_Not_CDC_Spanish = output_dir + input1_source + '_' + input2_source + \
@@ -199,16 +191,11 @@ with open(MGT_Diff_Single_Sheet, 'wt', encoding='utf-8', newline='') as file_out
     tsv_writer = csv.writer(file_out, delimiter='\t')
     tsv_writer.writerow([h for h in headers])
     tsv_writer.writerow(
-        ["Count of CDC English matches NIH English but not Spanish: ", count_english_term_exists])
-    tsv_writer.writerow(
         ["Count of CDC English has no NIH English Match: ", count_no_match_list_input1])
     tsv_writer.writerow(
         ["Count of NIH English has no CDC English Match: ", count_no_match_list_input2])
     tsv_writer.writerow(
         ["Count CDC and NIH Terms that match Both English and Spanish: ", count_terms_match])
-    for i in english_term_exists:
-        a = ['1'] + i
-        tsv_writer.writerow(a)
     for j in no_match_list_input1_item:
         b = ['2'] + j
         tsv_writer.writerow(b)
@@ -218,13 +205,6 @@ with open(MGT_Diff_Single_Sheet, 'wt', encoding='utf-8', newline='') as file_out
     for l in terms_match:
         d = ['4'] + l
         tsv_writer.writerow(d)
-
-with open(English_Not_Spanish, 'wt', encoding='utf-8', newline='') as file_out:
-    tsv_writer = csv.writer(file_out, delimiter='\t')
-    tsv_writer.writerow([h for h in headers])
-    tsv_writer.writerow(["Count: ", count_english_term_exists])
-    for i in english_term_exists:
-        tsv_writer.writerow(i)
 
 with open(CDC_English_Not_NIH_Spanish, 'wt', encoding='utf-8', newline='') as file_out:
     tsv_writer = csv.writer(file_out, delimiter='\t')
@@ -249,6 +229,6 @@ with open(Terms_Match, 'wt', encoding='utf-8', newline='') as file_out:
 
 
 with open(MGT_Diff_Single_Sheet_HTML, 'wt', encoding='utf-8', newline='') as file_out:
-    html = single_sheet_html(english_term_exists, no_match_list_input1_item, no_match_list_input2_item, terms_match,
-                             count_english_term_exists, count_no_match_list_input1, count_no_match_list_input2, count_terms_match)
+    html = single_sheet_html(headers, no_match_list_input1_item, no_match_list_input2_item, terms_match,
+                             count_no_match_list_input1, count_no_match_list_input2, count_terms_match)
     file_out.write(html)

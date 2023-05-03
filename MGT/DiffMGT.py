9f39e75bc155b3dde74b8576f53d9a631fa9357d
Mark Shellenberger <mshellenberger@flexion.us>
2023-05-03 10:38:31 -0400

    spanish mismatch
    added Spanish mismatch handling
    reduce complexity by using final output of inner list
    Heading changes
    return to top of document rather than return to outline
    commented out standard output and complex matching code for inner list (for potential reuse as debugging)

--- MGT/DiffMGT.py
+++ MGT/DiffMGT.py
@@ -31,17 +31,17 @@ input2_initial = open('\GitHub\markXLIX.github.io\MGT\A_SSA English-Spanish.tsv'
 
 #            <style>.row_highlight {background-color: gray;}</style>
 
-def single_sheet_html(headers, no_match_list_input1_item, no_match_list_input2_item, terms_match, input1_count, input2_count, match_count):
+def single_sheet_html(headers, no_match_list_input1_item, no_match_list_input2_item, terms_match, input1_count, input2_count, match_count, spanish_mismatch_count):
     html_start = f"""<html lang='en'>
             <meta charset='UTF-8'>
             <title>Multilingual Glossary Tool Diff of Glossaries - {input1_source} and {input2_source}</title>
             <meta name='viewport' content='width=device-width,initial-scale=1'>
             <head><link rel="stylesheet"  type="text/css" href="..\DiffMGT.css"></head>
             <body>
-                <h1>Multilingual Glossary Tool Diff of Glossaries</h1>
+                <h1 class='TOP'>Multilingual Glossary Tool Diff of Glossaries</h1>
                 """
     html_counter = single_sheet_html_count(
-        input1_count, input2_count, match_count)
+        input1_count, input2_count, match_count, spanish_mismatch_count)
     html_body = single_sheet_html_body(
         headers, no_match_list_input1_item, no_match_list_input2_item, terms_match)
     html_end = f"""
@@ -50,13 +50,15 @@ def single_sheet_html(headers, no_match_list_input1_item, no_match_list_input2_i
     return html_start + html_counter + html_body + html_end
 
 
-def single_sheet_html_count(input1_count, input2_count, match_count):
+def single_sheet_html_count(input1_count, input2_count, match_count, spanish_mismatch_count):
     html = f"""<div id='outline'>
         <p>
                 <ul>
                     <li><a href='#input1_no_match'>{input1_source} English - No Match to {input2_source} English</a> - {input1_count} hits</li>
-                    <li><a href='#input2_no_match'>{input2_source} - No Match to {input1_source}</a> - {input2_count} hits</li>
+                    <li><a href='#input2_no_match'>{input2_source} English - No Match to {input1_source} English</a> - {input2_count} hits</li>
                     <li><a href='#match'>Inputs Match</a> - {match_count} hits</li>
+                    <li><a href='#spanish_mismatch'>English Match, Spanish Does Not</a> - {spanish_mismatch_count} hits</li>
+
                 </ul>
             </p>
     </div>"""
@@ -67,7 +69,7 @@ def single_sheet_html_count(input1_count, input2_count, match_count):
 
 def single_sheet_html_body(headers, no_match_list_input1_item, no_match_list_input2_item, terms_match):
     # Input 1 vs Input 2
-    table_1 = f"""<div id='input1_no_match'><h2>no_match_list_input1_item - {input1_source} Has English Term That Does Not Exist In {input2_source}</h2>
+    table_1 = f"""<div id='input1_no_match'><h2>{input1_source} English - No Match to {input2_source} English</h2>
         <table><tr><th>{headers[0]}</th><th>{headers[1]}</th><th>{headers[2]}</th><th>{headers[3]}</th></tr>"""
     counter = 0
     highlight = ''
@@ -79,12 +81,12 @@ def single_sheet_html_body(headers, no_match_list_input1_item, no_match_list_inp
         table_1 = table_1 + term_row
         counter += 1
         highlight = ''
-    table_1 += "</table><p><a href='#outline'>Return to TOC</a></p><br/<br/></div>"
+    table_1 += "</table><p><a href='#TOP'>Return to TOP</a></p><br/<br/></div>"
     # reset counter for next table
     counter = 0
 
     # input 2 vs Input 1
-    table_2 = f"""<div id='input2_no_match'><h2>no_match_list_input2_item - {input2_source} - No Match to {input1_source}</h2>
+    table_2 = f"""<div id='input2_no_match'><h2>{input2_source} English - No Match to {input1_source} English</h2>
         <table><tr><th>{headers[0]}</th><th>{headers[1]}</th><th>{headers[2]}</th><th>{headers[3]}</th></tr>"""
     counter = 0
     highlight = ''
@@ -96,12 +98,12 @@ def single_sheet_html_body(headers, no_match_list_input1_item, no_match_list_inp
         table_2 = table_2 + term_row
         counter += 1
         highlight = ''
-    table_2 += "</table><p><a href='#outline'>Return to TOC</a></p><br/<br/></div>"
+    table_2 += "</table><p><a href='#TOP'>Return to TOP</a></p><br/<br/></div>"
     # reset counter for next table
     counter = 0
 
     # Matches
-    table_3 = f"""<div id='match'><h2>terms_match - {input1_source} and {input2_source} Match</h2>
+    table_3 = f"""<div id='match'><h2>Inputs Match</h2>
         <table><tr><th>{headers[0]}</th><th>{headers[1]}</th><th>{headers[2]}</th><th>{headers[3]}</th></tr>"""
     counter = 0
     highlight = ''
@@ -113,11 +115,11 @@ def single_sheet_html_body(headers, no_match_list_input1_item, no_match_list_inp
         table_3 = table_3 + term_row
         counter += 1
         highlight = ''
-    table_3 += "</table><p><a href='#outline'>Return to TOC</a></p><br/<br/></div>"
+    table_3 += "</table><p><a href='#TOP'>Return to TOP</a></p><br/<br/></div>"
     # reset counter for next table
     counter = 0
 
-    table_4 = f"""<div id='match'><h2>spanish_mismatch - {input1_source} and {input2_source} English Match - Spanish does not</h2>
+    table_4 = f"""<div id='spanish_mismatch'><h2>English Match, Spanish Does Not</h2>
         <table><tr><th>{headers[0]}</th><th>{headers[1]}</th><th>{headers[2]}</th><th>{headers[3]}</th></tr>"""
     counter = 0
     highlight = ''
@@ -129,11 +131,11 @@ def single_sheet_html_body(headers, no_match_list_input1_item, no_match_list_inp
         table_4 = table_4 + term_row
         counter += 1
         highlight = ''
-    table_4 += "</table><p><a href='#outline'>Return to TOC</a></p><br/<br/></div>"
+    table_4 += "</table><p><a href='#TOP'>Return to TOP</a></p><br/<br/></div>"
     # reset counter for next table
     counter = 0
 
-    return table_1 + table_2 + table_3 + table_4
+    return table_3 + table_4 + table_1 + table_2
 
 
 input2_list = []
@@ -191,8 +193,13 @@ for input1_item in input1_list:
         no_match_list_input1_item.append(
             [input1_item[0], input1_item[1], None, None])
 
-# Check for SSA terms that do not exist in CDC list
+# Remaining items in second list do not have a match in the first list.  Simply output
 for input2_item in input2_list:
+    no_match_list_input2_item.append(
+        [None, None, input2_item[0], input2_item[1]])
+
+# Check for SSA terms that do not exist in CDC list
+""" for input2_item in input2_list:
     match_found = False
     for input1_item in input1_list:
         if input1_item[0] != '':
@@ -209,15 +216,16 @@ for input2_item in input2_list:
                 break
     if not match_found:
         a = [None, None, input2_item[0], input2_item[1]]
-        no_match_list_input2_item.append(a)
+        no_match_list_input2_item.append(a) """
 
 # count of each output variable
 count_no_match_list_input1 = len(no_match_list_input1_item)
 count_no_match_list_input2 = len(no_match_list_input2_item)
 count_terms_match = len(terms_match)
+count_spanish_mismatch = len(spanish_mismatch)
 
-# print the results to screen
-print("\n", input1_source,
+# print the results to screen DEBUG
+""" print("\n", input1_source,
       "English/Spanish pair does not exist in", input2_source, ": ")
 for row in no_match_list_input1_item:
     print(row)
@@ -232,7 +240,7 @@ print("No match count:", count_no_match_list_input2)
 print("\nEnglish and Spanish Terms Match:")
 for row in terms_match:
     print(row)
-print("Match count:", count_terms_match)
+print("Match count:", count_terms_match) """
 
 # headers
 headers = ['CDC English', 'CDC Spanish', 'SSA English', 'SSA Spanish']
@@ -244,5 +252,5 @@ MGT_Diff_Single_Sheet_HTML = output_dir + input1_source + '_' + \
 # write the files
 with open(MGT_Diff_Single_Sheet_HTML, 'wt', encoding='utf-8', newline='') as file_out:
     html = single_sheet_html(headers, no_match_list_input1_item, no_match_list_input2_item, terms_match,
-                             count_no_match_list_input1, count_no_match_list_input2, count_terms_match)
+                             count_no_match_list_input1, count_no_match_list_input2, count_terms_match, count_spanish_mismatch)
     file_out.write(html)
